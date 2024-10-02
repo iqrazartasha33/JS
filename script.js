fdd9e571a0f5f036c8b07939385bd3cf1f150c9d
iqrazartasha33 <iqrazartasha33@gmail.com>
2024-10-02 23:22:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+//Ans 1
+
+var userName = prompt("What is your name?");
+alert("Hello, " + userName);
+
+//Ans 2
+
+var age = prompt("How old are you?");
+if (age === "") {
+    alert("No age provided.");
+} else {
+    alert("You are " + age + " years old.");
+}
